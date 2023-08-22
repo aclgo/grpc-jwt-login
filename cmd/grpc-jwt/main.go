db2ffb7c725ad984388b762ac6bb4a1ef4454db5
aclgo <marcellosanttos2014@gmail.com>
2023-08-21 21:58:52 -0300

    create simple studing example

--- cmd/grpc-jwt/main.go
+++ cmd/grpc-jwt/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/aclgo/grpc-jwt/config"
+	"github.com/aclgo/grpc-jwt/internal/server"
+	"github.com/aclgo/grpc-jwt/pkg/logger"
+	"github.com/aclgo/grpc-jwt/pkg/postgres"
+	rredis "github.com/aclgo/grpc-jwt/pkg/redis"
+)
 
 func main() {
 	fmt.Println("grpc-jwt")
+
+	cfg := config.Config{}
+	logger := logger.NewLogger(&cfg)
+
+	db, err := postgres.Connect(&cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	redisClient := rredis.NewRedisClient(&cfg)
+
+	server := server.NewServer(db, redisClient, logger, &cfg)
+
+	logger.Fatal(server.Run())
+
 }
