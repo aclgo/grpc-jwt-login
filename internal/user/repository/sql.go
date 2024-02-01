51a4f2f6b2f134e38dbd98e313d60cee1144d6a1
aclgo <marcellosanttos2014@gmail.com>
2024-02-01 19:19:32 -0300

    update sql

--- internal/user/repository/sql.go
+++ internal/user/repository/sql.go
@@ -2,7 +2,7 @@ package repository
 
 const (
 	queryAddUser = `INSERT INTO users (user_id, name, last_name, password, email,
-	role, verified, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) 
+	role, verified, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) 
 	RETURNING user_id, name, last_name, password, email, role, created_at, updated_at`
 
 	queryByID = `select user_id, name, last_name, password, email, role, verified, created_at,
